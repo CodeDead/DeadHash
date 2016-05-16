17e08bf2002556bff989ced6ea534826a842b8bf
CodeDead <admin@codedead.com>
2016-05-16 14:47:44 +0200

    * Added LanguageManager class

---
+++ DeadHash/Classes/LanguageManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeadHash.Classes
+{
+    /// <summary>
+    /// The Language Manager is responsible for changing the language of the GUI
+    /// </summary>
+    internal class LanguageManager
+    {
+    }
+}
