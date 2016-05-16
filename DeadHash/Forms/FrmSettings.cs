8b91dc8925cc42c0721cb8c79ef56309c295d785
CodeDead <admin@codedead.com>
2016-05-16 14:40:59 +0200

    * Minor optimisations * Added LoadLanguage

--- DeadHash/Forms/FrmSettings.cs
+++ DeadHash/Forms/FrmSettings.cs
@@ -1,10 +1,3 @@
-#region Copyright Syncfusion Inc. 2001-2016.
-// Copyright Syncfusion Inc. 2001-2016. All rights reserved.
-// Use of this code is subject to the terms of our license.
-// A copy of the current license can be obtained at any time by e-mailing
-// licensing@syncfusion.com. Any infringement will be prosecuted under
-// applicable laws. 
-#endregion
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -24,6 +17,7 @@ namespace DeadHash.Forms
         {
             InitializeComponent();
             LoadTheme();
+            LoadLanguage();
         }
 
         /// <summary>
@@ -34,6 +28,14 @@ namespace DeadHash.Forms
             MetroColor = Properties.Settings.Default.MetroColor;
             BorderColor = MetroColor;
             CaptionBarColor = MetroColor;
+        }
+
+        /// <summary>
+        /// CHange the GUI to match the current language, depending on the settings
+        /// </summary>
+        private void LoadLanguage()
+        {
+
         }
     }
 }
